b5044b49ea91bcb233529e0949165cb1daffd688
VladiMihaylenko <vxmihaylenko@gmail.com>
2019-04-01 11:54:07 +0300

    test

--- geometry/region2d/boost_concept.hpp
+++ geometry/region2d/boost_concept.hpp
@@ -3,16 +3,16 @@
 #include "geometry/point2d.hpp"
 #include "geometry/region2d.hpp"
 
-#if defined(__clang__)
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wshorten-64-to-32"
-#include "3party/boost/boost/polygon/detail/polygon_sort_adaptor.hpp"
-#include "3party/boost/boost/polygon/polygon.hpp"
-#pragma clang diagnostic pop
-#else
-#include "3party/boost/boost/polygon/detail/polygon_sort_adaptor.hpp"
-#include "3party/boost/boost/polygon/polygon.hpp"
-#endif
+//#if defined(__clang__)
+//#pragma clang diagnostic push
+//#pragma clang diagnostic ignored "-Wshorten-64-to-32"
+//#include <boost/polygon/detail/polygon_sort_adaptor.hpp>
+//#include <boost/polygon/polygon.hpp>
+//#pragma clang diagnostic pop
+//#else
+#include <boost/polygon/detail/polygon_sort_adaptor.hpp>
+#include <boost/polygon/polygon.hpp>
+//#endif
 
 #include <vector>
 
